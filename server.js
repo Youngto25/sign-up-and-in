fdd576b0596cc6bfd1939706dce272e628443e51
Blake <2979337714@qq.com>
2019-05-04 17:21:27 +0800

    add cookie

--- server.js
+++ server.js
@@ -12,6 +12,36 @@ var server = http.createServer(function(request,response){
 
   if(path === '/'){
     var string = fs.readFileSync('./homepage.html','utf8')
+    let cookies = request.headers.cookie.split(';');
+    let hash = {};
+    for(let i = 0; i <cookies.length; i++){
+      let parts = cookies[i].split('=');
+      let key = parts[0];
+      let value = parts[1];
+      hash[key] = value;
+    }
+    let email = hash.sign_in_email;
+    var users = fs.readFileSync('./database','utf8');
+        try{
+          users = JSON.parse(users);
+        }catch(exception){
+          users = [];
+        }
+    let foundUser;
+    for(let i=0;i<users.length;i++){
+      if(users[i].email === email){
+        foundUser = users[i];
+        break;
+      }
+    }
+    console.log(foundUser)
+    if(foundUser){
+      string = string.replace('__password__',foundUser.password);
+    }else{
+      string = string.replace('__password__','unknown');
+    }
+
+    response.statusCode = 200;
     response.setHeader('Content-Type', 'text/html;charset=utf-8')
     response.write(string)
     response.end()
@@ -70,35 +100,94 @@ var server = http.createServer(function(request,response){
           }
         }`);
       }else{
-        response.statusCode = 200;
-        
         var users = fs.readFileSync('./database','utf8');
         try{
           users = JSON.parse(users);
         }catch(exception){
           users = [];
         }
-        users.push({email: email,password: password});
-        var usersString = JSON.stringify(users);//将对象users转换成字符串
-        fs.writeFileSync('./database',usersString);
-       
+        //判断email在数据库中是否已经存在
+        let inUse = false;
+        for(let i=0; i<users.length; i++){
+          let user = users[i];
+          if(user.email === email){
+            inUse = true;
+            break;
+          }
+        }
+        if(inUse){
+          response.statusCode = 400;
+          response.write(`{
+            "errors":{
+              "email":"inuse"
+            }
+          }`);
+        }else{
+          users.push({email: email,password: password});
+          var usersString = JSON.stringify(users);//将对象users转换成字符串
+          fs.writeFileSync('./database',usersString);  
+          response.statusCode = 200;
+        } 
       }
       response.end()
     });
 
     
   }else if(path === '/signin' && method === 'GET'){
+    
     var string = fs.readFileSync('./signin.html','utf8')
     response.statusCode = 200
     response.setHeader('Content-Type', 'text/html;charset=utf-8')
     response.write(string)
     response.end()
+  
   }else if(path === '/signin' && method === 'POST'){
-    var string = fs.readFileSync('./signin.html','utf8')
-    response.statusCode = 200
-    response.setHeader('Content-Type', 'text/html;charset=utf-8')
-    response.write(string)
-    response.end()
+    
+    let body = [];
+    request.on('data',(chunk) =>{
+      body.push(chunk);
+    }).on('end', () =>{
+      body = Buffer.concat(body).toString();
+      let strings = body.split('&');
+      let hash = {};
+      strings.forEach((string) => { //['email=29']
+        let parts = string.split('=');  //['email','29']
+        let key = parts[0];
+        let value = parts[1];
+        hash[key] = value;
+      });
+      let {email,password} = hash;//ES6
+      var users = fs.readFileSync('./database','utf8');
+        try{
+          users = JSON.parse(users);
+        }catch(exception){
+          users = [];
+        }
+        //判断email在数据库中是否已经存在
+        let found = false;
+        for(let i=0; i<users.length; i++){
+          if(users[i].email === email && users[i].password === password){
+              found = true;
+              break;
+          }
+        }
+        if(found){
+          //Set-Cookie: <cookie-name>=<cookie-value> 
+          response.setHeader('Set-Cookie',`sign_in_email=${email}`);
+          response.statusCode = 200;
+        }else{
+          response.statusCode = 401;
+          response.setHeader('Content-Type','application/json;charset=utf-8');
+          response.write(`{
+            "errors":{
+              "password":"unmatch"
+            }
+          }`);
+        }
+        response.end()
+    })
+    
+  
   }else{
     response.statusCode = 400
     response.setHeader('Content-Type', 'text/html;charset=utf-8')
